fc532243d407f613baa5f5ad36ad586a002d9a8e
Daniil <kashel369@bk.ru>
2023-04-10 21:53:58 +0300

    fix port

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,6 +9,6 @@ async function bootstrap() {
     cors: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 4000);
 }
 bootstrap();
